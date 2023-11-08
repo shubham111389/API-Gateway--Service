8d1c314c38beb5dde0ee82ab18583db99e7bd65b
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-11-08 18:30:55 +0530

    added disauthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const { AuthRequestMiddlewares } = require('../../middlewares');
 
 const { InfoController } = require('../../controllers');
 
 const router = express.Router();
 const userRouter = require('./user-routes');
 
+router.get('/info',AuthRequestMiddlewares.checkAuth, InfoController.info);
 
-router.get('/info', InfoController.info);
-router.use('/signup', userRouter)
 router.use('/user', userRouter)
 
 
--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -18,9 +18,19 @@ function createToken(input) {
         console.log(error);
         throw error;
     }
+    function verifyToken(token) {
+        try {
+            return jwt.verify(token, ServerConfig.JWT_SECRET);
+        } catch(error) {
+            console.log(error);
+            throw error;
+        }
+    }
+    
 }
 
 module.exports = {
     checkPassword,
-    createToken
+    createToken,
+    verifyToken
 }
