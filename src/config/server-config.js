f688a22b75d27ef1d4ebddc19d2997ecea22f0fd
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-11-09 18:01:43 +0530

    Added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,6 @@ module.exports = {
     PORT: process.env.PORT,
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    BOOKING_SERVICE: process.env.BOOKING_SERVICE
 }
